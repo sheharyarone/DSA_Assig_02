f43fcbd299d3824e04525e13d0a3d8882afe6ddf
Sheharyar <sheharyarone@gmail.com>
2022-11-12 01:54:55 +0500

    error when printing array in fucntion suing d ptr

--- Task3_Static_Array.cpp
+++ Task3_Static_Array.cpp
@@ -3,55 +3,89 @@
 #include <time.h>
 using namespace std;
 
-int arrCheck[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-int arr[4][4] = {{1, 0, 0, 0}, {1, 1, 1, 1}, {1, 0, 0, 1}, {1, 1, 0, 1}};
-
-bool findPath(int x, int y, int endX, int endY, int size)
+bool findPath(int x, int y, int endX, int endY, int **arr, int size)
 {
-    arrCheck[x][y] = 1;
-
-    if (x == endX && y == endY)
-    {
-        return true;
-    }
-    if (x + 1 < size && arr[x + 1][y] == 1 && !arrCheck[x + 1][y])
-    {
-        findPath(x + 1, y, endX, endY, size);
-    }
-    if (y + 1 < size && arr[x][y + 1] == 1 && !arrCheck[x][y + 1])
-    {
-        findPath(x, y + 1, endX, endY, size);
-    }
-    if (y - 1 >= 0 && arr[x][y - 1] == 1 && !arrCheck[x][y - 1])
-    {
-        findPath(x, y - 1, endX, endY, size);
-    }
-    if (x - 1 >= 0 && arr[x - 1][y] == 1 && !arrCheck[x - 1][y])
-    {
-        findPath(x - 1, y, endX, endY, size);
-    }
-   
-}
-
-int main()
-{
-    findPath(0, 0, 3, 3, 4);
-    if(arrCheck[3][3])
-    {
-        cout << "U GoT PATH." << endl;
-    }
-    else
-    {
-        cout << "YOU DON'T." << endl;
-    }
-
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
+        arr[x][y] = -1;
+        
+        if (x == endX && y == endY)
         {
-            cout << arrCheck[i][j] << " ";
+            return true;
         }
-        cout << endl;
+        if (x + 1 < size)
+        {
+            if (arr[x + 1][y] == 1)
+                findPath(x + 1, y, endX, endY, arr, size);
+        }
+        if (y + 1 < size)
+        {
+            if (arr[x][y + 1] == 1)
+                findPath(x, y + 1, endX, endY, arr, size);
+        }
+        if (y - 1 >= 0)
+        {
+            if (arr[x][y - 1] == 1)
+                findPath(x, y - 1, endX, endY, arr, size);
+        }
+        if (x - 1 >= 0)
+        {
+            if (arr[x - 1][y] == 1)
+                findPath(x - 1, y, endX, endY, arr, size);
+        }
+        if (arr[endX][endY] == -1)
+            return true;
+        return false;
     }
-    return 0;
-}
+
+    int main()
+    {
+        srand(time(NULL));
+        int *arr = new int[4 * 4];
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                *(arr + i * 4 + j) = rand() % 2;
+            }
+        }
+        cout << "MATRIX WHICH IS FORMED : " << endl;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cout << *(arr + i * 4 + j) << " ";
+            }
+            cout << endl;
+        }
+
+        int x, y, endX, endY;
+
+        x = rand() % 4;
+        y = rand() % 4;
+        while (*(arr + x * 4 + y) != 1)
+        {
+            x = rand() % 4;
+            y = rand() % 4;
+        }
+        endX = rand() % 4;
+        endY = rand() % 4;
+        while (*(arr + endX * 4 + endY) != 1)
+        {
+            endX = rand() % 4;
+            endY = rand() % 4;
+        }
+
+        cout << x << y << " " << endl;
+        cout << endX << endY << endl;
+        cout << findPath(x, y, endX, endY, &arr, 4) << endl;
+
+        cout << "AFTER TRAVERSING" << endl;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cout << *(arr + i * 4 + j) << " ";
+            }
+            cout << endl;
+        }
+        return 0;
+    }
