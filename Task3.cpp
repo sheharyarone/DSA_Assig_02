b4380f0dfd96866546d1f2786b7b8741a47b1f64
Sheharyar <sheharyarone@gmail.com>
2022-11-12 02:33:26 +0500

    working with dynamic allocation

--- Task3.cpp
+++ Task3.cpp
@@ -3,96 +3,104 @@
 #include <time.h>
 using namespace std;
 
-bool range(int x, int y, int *arr, int size)
+bool findPath(int x, int y, int endX, int endY, int **&arr, int size)
 {
-    return (*(arr + x + 1 * size + y) == 1) ||
-           (*(arr + x * size + y + 1) == 1) ||
-           (*(arr + x * size + y - 1) == 1) ||
-           (*(arr + x - 1 * size + y) == 1);
-}
-
-bool findPath(int x, int y, int endX, int endY, int *arr, int size)
-{
-    bool valid = false;
-
+    arr[x][y] = -1;
     if (x == endX && y == endY)
     {
         return true;
     }
-    if (x + 1 < size && (*(arr + x + 1 * size + y) == 1) && range(x + 1, y, arr, size))
-    {
-        return findPath(x + 1, y, endX, endY, arr, size);
-    }
-    else if (y + 1 < size && (*(arr + x * size + y + 1) == 1) && range(x, y + 1, arr, size))
+    if (x + 1 < size && arr[x + 1][y] == 1 && arr[endX][endY] != -1)
     {
 
-        return findPath(x, y + 1, endX, endY, arr, size);
+        findPath(x + 1, y, endX, endY, arr, size);
     }
-    else if (y - 1 >= 0 && (*(arr + x * size + y - 1) == 1) && range(x, y - 1, arr, size))
+    if (y + 1 < size && arr[x][y + 1] == 1 && arr[endX][endY] != -1)
     {
 
-        return findPath(x, y - 1, endX, endY, arr, size);
+        findPath(x, y + 1, endX, endY, arr, size);
     }
-    else if (x - 1 >= 0 && (*(arr + x - 1 * size + y) == 1) && range(x - 1, y, arr, size))
+    if (y - 1 >= 0 && arr[x][y - 1] == 1 && arr[endX][endY] != -1)
     {
 
-        return findPath(x - 1, y, endX, endY, arr, size);
+        findPath(x, y - 1, endX, endY, arr, size);
     }
-
-    else
+    if (x - 1 >= 0 && arr[x - 1][y] == 1 && arr[endX][endY] != -1)
     {
-        return false;
+
+        findPath(x - 1, y, endX, endY, arr, size);
+    }
+    if (arr[endX][endY] == -1)
+        return true;
+    return false;
+}
+void makeMatrix(int **arr, int size)
+{
+    srand(time(NULL));
+    for (int i = 0; i < 4; i++)
+    {
+        arr[i] = new int[4];
+    }
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            arr[i][j] = rand() % 2;
+        }
+    }
+}
+void printArray(int **arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
 }
 
 int main()
 {
-    srand(time(NULL));
-    int *arr = new int[4 * 4];
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            *(arr + i * 4 + j) = rand() % 2;
-        }
-    }
+    int size = 4;
+    int **arr = new int *[size];
+    makeMatrix(arr, size);
+    cout << "MATRIX WHICH IS FORMED : " << endl;
+    printArray(arr, size);
 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout << *(arr + i * 4 + j) << " ";
-        }
-        cout << endl;
-    }
-
+    // PICKING TWO RANDOM POINTS HAVING NO WALL
     int x, y, endX, endY;
 
     x = rand() % 4;
     y = rand() % 4;
-    while (*(arr + x * 4 + y) != 1)
+    while (arr[x][y] != 1)
     {
         x = rand() % 4;
         y = rand() % 4;
     }
     endX = rand() % 4;
     endY = rand() % 4;
-    while (*(arr + endX * 4 + endY) != 1)
+    while (arr[endX][endY] != 1)
+
     {
         endX = rand() % 4;
         endY = rand() % 4;
     }
-    cout << x << y << " " << *(arr + endX * 4 + endY) << endl;
-    cout << endX << endY << " " << *(arr + x * 4 + y) << endl;
+    // RANDOM POINTS SELECTED
+    
+    cout << "FIRST POINT : " << x << y << " " << endl;
+    cout << "SECOND POINT : " << endX << endY << endl;
+    cout << "RESULT OF OUR FUNCTION : " << findPath(x, y, endX, endY, arr, 4) << endl;
 
-    if (x < endX)
+    cout << "AFTER TRAVERSING" << endl;
+    for (int i = 0; i < 4; i++)
     {
-        cout << findPath(x, y, endX, endY, arr, 4);
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
     }
-    else
-    {
-        cout << findPath(endX, endY, x, y, arr, 4);
-    }
-
     return 0;
 }
